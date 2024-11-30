5559e2f377c97e6b59fb810b70207d293daed9d1
capacity-tighe-begley <156465174+capacity-tighe-begley@users.noreply.github.com>
2024-11-29 23:10:29 -0600

    Update OOBE.ps1

--- OOBE.ps1
+++ OOBE.ps1
@@ -244,30 +244,6 @@ function Step-oobeUpdateWindows {
         }
     }
 }
-function Invoke-Webhook {
-    $BiosSerialNumber = Get-MyBiosSerialNumber
-    $ComputerManufacturer = Get-MyComputerManufacturer
-    $ComputerModel = Get-MyComputerModel
-    
-    $URI = 'https://XXXX.webhook.office.com/webhookb2/YYYY'
-    $JSON = @{
-        "@type"    = "MessageCard"
-        "@context" = "<http://schema.org/extensions>"
-        "title"    = 'OSDCloud Information'
-        "text"     = "The following client has been successfully deployed:<br>
-                    BIOS Serial Number: **$($BiosSerialNumber)**<br>
-                    Computer Manufacturer: **$($ComputerManufacturer)**<br>
-                    Computer Model: **$($ComputerModel)**"
-        } | ConvertTo-JSON
-        
-        $Params = @{
-        "URI"         = $URI
-        "Method"      = 'POST'
-        "Body"        = $JSON
-        "ContentType" = 'application/json'
-        }
-        Invoke-RestMethod @Params | Out-Null
-}
 
 function Step-oobeRestartComputer {
     [CmdletBinding()]
