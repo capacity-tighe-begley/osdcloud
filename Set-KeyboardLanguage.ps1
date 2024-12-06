9ba077024b5b8aea01c5d14c56ecbcb59b472938
capacity-tighe-begley <156465174+capacity-tighe-begley@users.noreply.github.com>
2024-12-06 16:31:38 -0600

    update

--- Set-KeyboardLanguage.ps1
+++ Set-KeyboardLanguage.ps1
@@ -11,18 +11,18 @@ $env:Path = $env:Path + ";C:\Program Files\WindowsPowerShell\Scripts"
 $Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-Set-KeyboardLanguage.log"
 Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
 
-Write-Host -ForegroundColor Green "Set keyboard language to de-CH"
+Write-Host -ForegroundColor Green "Set keyboard language to en-US"
 Start-Sleep -Seconds 5
 
 $LanguageList = Get-WinUserLanguageList
 
-$LanguageList.Add("de-CH")
+$LanguageList.Add("en-US")
 Set-WinUserLanguageList $LanguageList -Force
 
 Start-Sleep -Seconds 5
 
 $LanguageList = Get-WinUserLanguageList
-$LanguageList.Remove(($LanguageList | Where-Object LanguageTag -like 'de-DE'))
+$LanguageList.Remove(($LanguageList | Where-Object LanguageTag -like 'en-US'))
 Set-WinUserLanguageList $LanguageList -Force
 
 $LanguageList = Get-WinUserLanguageList
